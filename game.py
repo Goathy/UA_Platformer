db4256f1b4260dd6faaba91ccea01e8abaea5563
Jakub Gałecki <goathy.dev@gmail.com>
2023-05-13 13:33:09 +0200

    apply formatting

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import pygame
 import os
 import os.path
 
-BG_COLOR=(255, 255, 255)
+BG_COLOR = (255, 255, 255)
 
 WIDTH = 1000
 HEIGHT = 800
@@ -14,18 +14,18 @@ PLAYER_VEL = 30
 
 pygame.init()
 
-pygame.display.set_caption('platformer')
+pygame.display.set_caption("platformer")
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 def get_background(name):
     image = pygame.image.load(os.path.join(name))
-    _,_, width, height = image.get_rect()
+    _, _, width, height = image.get_rect()
 
     tiles = []
 
-    for i in range(WIDTH // width +1):
+    for i in range(WIDTH // width + 1):
         for j in range(HEIGHT // height + 1):
             pos = [i * width, j * height]
             tiles.append(pos)
@@ -39,11 +39,12 @@ def draw(window, background, bg_image):
 
     pygame.display.update()
 
+
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Blue.png")
 
-    run = True 
+    run = True
     while run:
         clock.tick(FPS)
 
@@ -52,11 +53,12 @@ def main(window):
                 case pygame.QUIT:
                     run = False
                     break
-                    
+
         draw(window, background, bg_image)
 
     pygame.quit()
     quit()
+
 
 if __name__ == "__main__":
     main(window)
